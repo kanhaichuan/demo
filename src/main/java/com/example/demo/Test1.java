c2d4c36ed8d272c7e8310434b297fffca1a70f5e
binjie <877130039@qq.com>
2019-07-16 14:54:00 +0800

    第一次

---
+++ src/main/java/com/example/demo/Test1.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+/**
+ * ClassName:Test1
+ * Package:com.example.demo
+ * Description:
+ *
+ * @Date:2019/7/1614:51
+ * @Author:877130039@qq.com
+ */
+public class Test1 {
+    public void test1(){
+        System.out.println(111);
+    }
+}
